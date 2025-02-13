cf8c5f1c96419b1276b18dacb6638ced8922e09f
Matthew Hambley <matthew.hambley@metoffice.gov.uk>
2025-02-13 11:24:53 +0000

    RSync tests updated.

--- tests/unit_tests/tools/test_rsync.py
+++ tests/unit_tests/tools/test_rsync.py
@@ -3,17 +3,19 @@
 # For further details please refer to the file COPYRIGHT
 # which you should have received as part of this distribution
 ##############################################################################
+"""
+Tests RSync file tree synchronisation tool.
+"""
+from fab.tools.category import Category
+from fab.tools.rsync import Rsync
 
-'''Tests the rsync implementation.
-'''
-
-from unittest import mock
-
-from fab.tools import (Category, Rsync)
+from pytest_subprocess.fake_process import FakeProcess
 
 
-def test_ar_constructor():
-    '''Test the rsync constructor.'''
+def test_constructor():
+    """
+    Tests default constructor
+    """
     rsync = Rsync()
     assert rsync.category == Category.RSYNC
     assert rsync.name == "rsync"
@@ -21,39 +23,46 @@ def test_ar_constructor():
     assert rsync.flags == []
 
 
-def test_rsync_check_available():
-    '''Tests the is_available functionality.'''
+def test_check_available(fake_process: FakeProcess) -> None:
+    """
+    Tests availability checking functionality.
+    """
+    def not_found(process):
+        process.returncode = 1
+        raise FileNotFoundError("RSync executable missing")
+
+    fake_process.register(['rsync', '--version'], stdout='1.2.3')
+    fake_process.register(['rsync', '--version'], callback=not_found)
+
     rsync = Rsync()
-    with mock.patch("fab.tools.tool.Tool.run") as tool_run:
-        assert rsync.check_available()
-    tool_run.assert_called_once_with("--version")
+    assert rsync.check_available()
 
     # Test behaviour if a runtime error happens:
-    with mock.patch("fab.tools.tool.Tool.run",
-                    side_effect=RuntimeError("")) as tool_run:
-        assert not rsync.check_available()
+    assert not rsync.check_available()
+
+    assert [call for call in fake_process.calls] == [
+        ['rsync', '--version'],
+        ['rsync', '--version']
+    ]
 
 
-def test_rsync_create():
-    '''Test executing an rsync, and also make sure that src always
-    end on a '/'.
-    '''
+def test_rsync_create(fake_process: FakeProcess) -> None:
+    """
+    Tests performing a sync. Ensure source always ends with a '/'.
+    """
+    with_command = ['rsync', '--times', '--links', '--stats', '-ru', '/src/', '/dst']
+    fake_process.register(with_command)
+    without_command = ['rsync', '--times', '--links', '--stats', '-ru', '/src/', '/dst']
+    fake_process.register(without_command)
+
     rsync = Rsync()
 
     # Test 1: src with /
-    mock_result = mock.Mock(returncode=0)
-    with mock.patch('fab.tools.tool.subprocess.run',
-                    return_value=mock_result) as tool_run:
-        rsync.execute(src="/src/", dst="/dst")
-    tool_run.assert_called_with(
-        ['rsync', '--times', '--links', '--stats', '-ru', '/src/', '/dst'],
-        capture_output=True, env=None, cwd=None, check=False)
+    rsync.execute(src="/src/", dst="/dst")
 
     # Test 2: src without /
-    mock_result = mock.Mock(returncode=0)
-    with mock.patch('fab.tools.tool.subprocess.run',
-                    return_value=mock_result) as tool_run:
-        rsync.execute(src="/src", dst="/dst")
-    tool_run.assert_called_with(
-        ['rsync', '--times', '--links', '--stats', '-ru', '/src/', '/dst'],
-        capture_output=True, env=None, cwd=None, check=False)
+    rsync.execute(src="/src", dst="/dst")
+
+    assert [call for call in fake_process.calls] == [
+        with_command, without_command
+    ]
